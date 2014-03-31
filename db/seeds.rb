ea8fbb18425c4ac74750290b9d7dbf59a0aecf67
GREGORY MALCOLM <greg.j.malcolm@chase.com>
2014-03-31 17:27:57 -0400

    Tweaked card viewer. Up to 3 stories

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,25 @@ if Card.count > 0
   puts "Looks like you already have cards. Not regenerating them"
 else
   Card.create done: false,
-              title: "List Actors",
-              description: "The Actors link takes us to a page marked 'Not implemented'. This page should list a full set of actors",
+              title: "List actors",
+              description: "The actors link takes us to a page marked 'Not implemented'. This page should list a full set of actors",
               difficulty: DIFFICULTY_1,
               comments: "We're implementing the /#/actors url specifically. Should look similar to /#/movies",
               clue1: "The implementation is almost identical to movies counterpart"
+  Card.create done: false,
+              title: "Actor links should go show actor panel",
+              description: "Clicking on each each Actor name should cause an actor pane to show up on the right.",
+              difficulty: DIFFICULTY_1,
+              comments: "Dependency: 'List Actors' card. Similar to how the movie links work.",
+              clue1: "Use the 'link-to' template helper.",
+              clue2: "Don't forget to create a router resource called 'actor'"
+  Card.create done: false,
+              title: "Show actor details",
+              description: "For each actor displayed we want to see the actors name, and date of birth.",
+              difficulty: DIFFICULTY_1,
+              comments: "Dependency: 'List Actors' card. http://localhost:3000/#/actors/200 would take you to the first actor",
+              clue1: "Don't forget to create a router resource called 'actor'",
+              clue2: "Look in models/actor.js.coffee to see actor attributes",
+              clue3: "Similar to viewing a movie"
   puts "Created a full set of cards."
 end
